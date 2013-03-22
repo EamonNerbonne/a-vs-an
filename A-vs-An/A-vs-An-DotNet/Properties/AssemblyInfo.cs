e5f7ce5aa7df3485d0cdd598b18577c55680543c
Eamon Nerbonne <eamon@nerbonne.org>
2013-03-22 13:16:40 +0100

    Create nuget package AvsAn
    --HG--
    extra : rebase_source : 379fd2872e6ca42989275f4756e2c724b6781c58

--- A-vs-An/A-vs-An-DotNet/Properties/AssemblyInfo.cs
+++ A-vs-An/A-vs-An-DotNet/Properties/AssemblyInfo.cs
@@ -5,14 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("A-vs-An-DotNet")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("A-vs-An-DotNet")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("AvsAn")]
+[assembly: AssemblyDescription("Find the english language indeterminate article ('a' or 'an') for a word.  Based on real usage patterns extracted from the wikipedia text dump; can therefore even deal with tricky edge cases such as acronyms (FIAT vs. FAA, NASA vs. NSA) and odd symbols.  (Requires .NET 2.0 or higher)")]
+[assembly: AssemblyCompany("Eamon Nerbonne")]
+[assembly: AssemblyProduct("AvsAn")]
+[assembly: AssemblyCopyright("Copyright Eamon Nerbonne ©  2013")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -33,4 +30,3 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
