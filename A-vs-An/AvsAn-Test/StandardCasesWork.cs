8fa771e5154dc7a555cffaafed0a0f885cf0baea
Eamon Nerbonne <eamon@nerbonne.org>
2013-03-22 12:07:59 +0100

    Added initial implementation of .NET AvsAn
    --HG--
    extra : rebase_source : a3cd8c266ea055e52db79a5239d005cf67ff8212

---
+++ A-vs-An/AvsAn-Test/StandardCasesWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AvsAnLib;
+using ExpressionToCodeLib;
+using NUnit.Framework;
+
+namespace AvsAn_Test
+{
+    public class StandardCasesWork
+    {
+
+		[TestCase("an", "unanticipated result")]
+		[TestCase("a", "unanimous vote")]
+		[TestCase("an", "honest decision")]
+		[TestCase("a", "honeysuckle shrub")]
+		[TestCase("an", "0800 number")]
+		[TestCase("an", "∞ of oregano")]
+		[TestCase("a", "NASA scientist")]
+		[TestCase("an", "NSA analyst")]
+		[TestCase("a", "FIAT car")]
+		[TestCase("an", "FAA policy")]
+		public void DoTest(string article, string word) {
+			PAssert.That(() => AvsAn.Query(word).Article == article);
+		}
+    }
+}
