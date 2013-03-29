14a93430c1d44b19a633da28515a2b1691929b0a
Eamon Nerbonne <eamon@nerbonne.org>
2013-03-29 19:44:28 +0100

    More memory efficient implementation by using a seperate mutable construction phase and a denser final lookup phase.  Initialization is no slower.
    --HG--
    extra : rebase_source : 8f5cbfc7b0c78404089e1958f65bd147fa0e5a93

--- A-vs-An/PerfTest/Program.cs
+++ A-vs-An/PerfTest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 using AvsAnLib;
@@ -15,14 +16,27 @@ namespace PerfTest {
 			var tr = new Dictionary<string, int> { { "a", 0 }, { "an", 1 } };
 			long sum = 0;
 			Stopwatch sw = Stopwatch.StartNew();
-			for (var k = 0; k < 50; k++) {
+			var _ = AvsAn.Query("example").Article;
+			var init = sw.Elapsed;
+			Console.WriteLine("initialization took " + init.TotalMilliseconds);
+			sw.Restart();
+			const int iters = 50;
+			for (var k = 0; k < iters; k++) {
 				for (var i = 0; i < words.Length; i++)
 					sum += AvsAn.Query(words[i]).Article == "an" ? 1 : 0;
 				for (var i = words.Length - 1; i >= 0; i--)
 					sum += AvsAn.Query(words[i]).Article == "an" ? 1 : 0;
 			}
 			var duration = sw.Elapsed.TotalMilliseconds;
-			Console.WriteLine(sum + " / " + words.Length + " (" + ((double)sum / words.Length / 10) + ") an rate.");
+			var clockrateMHz = 
+				new ManagementObjectSearcher(new ObjectQuery("SELECT CurrentClockSpeed FROM Win32_Processor")).Get()
+				.Cast<ManagementObject>().Select(mo => Convert.ToDouble(mo["CurrentClockSpeed"])).Max();
+			Console.WriteLine(sum + " / " + words.Length + " (" + ((double)sum / words.Length / iters / 2) + ") an rate.");
+			var microseconds = duration / words.Length / iters / 2 * 1000.0;
+			Console.WriteLine(microseconds * 1000.0 + " nanoseconds per lookup");
+
+			Console.WriteLine(clockrateMHz *microseconds +" cycles per lookup @ " + clockrateMHz+"MHz");
+			
 			Console.WriteLine("took " + duration);
 		}
 	}
