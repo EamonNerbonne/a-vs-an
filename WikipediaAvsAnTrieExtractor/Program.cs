a573075d8b0bc7bb412250e21f6b00cc78676c3b
Eamon Nerbonne <eamon@nerbonne.org>
2014-06-12 16:01:34 +0200

    Split fairly large main function (56 lines) into several helper methods; now it's 15 lines.

--- WikipediaAvsAnTrieExtractor/Program.cs
+++ WikipediaAvsAnTrieExtractor/Program.cs
@@ -12,81 +12,77 @@ using System.Diagnostics;
 
 namespace AvsAnTrie {
     static class Program {
-		const string dictFileName = "english.ngl";
-		static void Main() {
-			var exeFile = new FileInfo(Assembly.GetEntryAssembly().Location);
-			var dir = exeFile.Directory;
-			while (dir != null && !dir.GetFiles(dictFileName).Any())
-				dir = dir.Parent;
-			RegexTextUtils.LoadDictionary(dir.GetFiles(dictFileName).First());
+        const string dictFileName = "english.ngl";
+        static void Main() {
+            var exeFile = new FileInfo(Assembly.GetEntryAssembly().Location);
+            var dir = exeFile.Directory;
+            while (dir != null && !dir.GetFiles(dictFileName).Any())
+                dir = dir.Parent;
+            RegexTextUtils.LoadDictionary(dir.GetFiles(dictFileName).First());
+            var wikiPath = new FileInfo(@"F:\wikipedia\enwiki-latest-pages-articles.xml");
+            var wikiPageQueue = LoadWikiPagesAsync(wikiPath);
+            var entriesTodo = ExtractAvsAnSightingsAsync(wikiPageQueue);
+            var trieBuilder = BuildAvsAnTrie(entriesTodo, () => wikiPageQueue.Count);
+            AnnotatedTrie result = trieBuilder.Result;
+            Console.WriteLine("Before simplification: trie of # nodes" + trieBuilder.Result.CountParallel);
+            result.Simplify();
+            File.AppendAllText(@"D:\test.log", result.Readable());
+            Console.WriteLine("After simplification: trie of # nodes" + trieBuilder.Result.CountParallel);
+        }
 
-			var wikiPath = new FileInfo(@"F:\wikipedia\enwiki-latest-pages-articles.xml");
-			var entriesTodo = new BlockingCollection<Entry[]>(3000);
-			var pagesTodo = new BlockingCollection<XElement>(3000);
-			int wordCount = 0;
-			Task.Factory.StartNew(() =>
-			{
-			    Stopwatch sw = Stopwatch.StartNew();
-				while (!entriesTodo.IsCompleted) {
-					Thread.Sleep(5000);
-					Console.WriteLine("Entrycache: " + entriesTodo.Count + "; pagecache: " + pagesTodo.Count + "; wordcount: " + wordCount + "; words/sec: " + (wordCount/sw.Elapsed.TotalSeconds).ToString("f1") );
-				}
-			});
+        static BlockingCollection<AvsAnSighting[]> ExtractAvsAnSightingsAsync(BlockingCollection<XElement> wikiPageQueue) {
+            var entriesTodo = new BlockingCollection<AvsAnSighting[]>(3000);
 
-			Task.Factory.StartNew(() => {
-				using (var stream = wikiPath.OpenRead())
-				using (var reader = XmlReader.Create(stream))
-					while (reader.Read())
-						if (reader.NodeType == XmlNodeType.Element && reader.LocalName == "page")
-							pagesTodo.Add((XElement)XNode.ReadFrom(reader));
-				pagesTodo.CompleteAdding();
-			});
+            var sightingExtractionTask = Task.WhenAll(
+                Enumerable.Range(0, 32).Select(i =>
+                    Task.Factory.StartNew(() => {
+                        var ms = new RegexTextUtils();
+                        foreach (var page in wikiPageQueue.GetConsumingEnumerable())
+                            entriesTodo.Add(ms.FindAvsAnSightings(page));
+                    }, TaskCreationOptions.LongRunning)
+                    ).ToArray()
+                );
 
-			var trieBuilder = Task.Factory.StartNew(() => {
-				var trie = new AnnotatedTrie();
-				foreach (var entries in entriesTodo.GetConsumingEnumerable())
-					foreach (var entry in entries) {
-						trie.AddEntry(entry.PrecededByAn, entry.Word, 0);
-						wordCount++;
-					}
-				return trie;
-			});
+            sightingExtractionTask.ContinueWith(_ => entriesTodo.CompleteAdding());
+            return entriesTodo;
+        }
 
-			Task.WaitAll(
-				Enumerable.Range(0, 32).Select(i =>
-					Task.Factory.StartNew(() => ExtractEntriesFromPages(pagesTodo, entriesTodo))
-				).ToArray()
-			);
+        static Task<AnnotatedTrie> BuildAvsAnTrie(BlockingCollection<AvsAnSighting[]> entriesTodo, Func<int> wikiPageQueueLength) {
+            int wordCount = 0;
+            Task.Factory.StartNew(() => {
+                Stopwatch sw = Stopwatch.StartNew();
+                while (!entriesTodo.IsCompleted) {
+                    Thread.Sleep(5000);
+                    Console.WriteLine("Entrycache: " + entriesTodo.Count + "; pagecache: " + wikiPageQueueLength() +
+                                      "; wordcount: " + wordCount + "; words/sec: " +
+                                      (wordCount / sw.Elapsed.TotalSeconds).ToString("f1"));
+                }
+            }, TaskCreationOptions.LongRunning);
 
-			//Parallel.ForEach(pagesTodo.GetConsumingEnumerable(), new ParallelOptions { MaxDegreeOfParallelism = 128 }, () => new WikiMarkupStripper(), (page, state, ms) => {
-			//    ProcessPage(entriesTodo, page, ms);
-			//    return ms;
-			//}, ms => { });
+            var trieBuilder = Task.Factory.StartNew(() => {
+                var trie = new AnnotatedTrie();
+                foreach (var entries in entriesTodo.GetConsumingEnumerable())
+                    foreach (var entry in entries) {
+                        trie.AddEntry(entry.PrecededByAn, entry.Word, 0);
+                        wordCount++;
+                    }
+                return trie;
+            }, TaskCreationOptions.LongRunning);
+            return trieBuilder;
+        }
 
-			entriesTodo.CompleteAdding();
-			AnnotatedTrie result = trieBuilder.Result;
-			Console.WriteLine("Before simplification: trie of # nodes" + trieBuilder.Result.CountParallel);
-			result.Simplify();
-			File.AppendAllText(@"D:\test.log", result.Readable());
-			Console.WriteLine("After simplification: trie of # nodes" + trieBuilder.Result.CountParallel);
-		}
+        static BlockingCollection<XElement> LoadWikiPagesAsync(FileInfo wikiPath) {
+            var pagesTodo = new BlockingCollection<XElement>(3000);
 
-		static void ExtractEntriesFromPages(BlockingCollection<XElement> pagesTodo, BlockingCollection<Entry[]> entriesTodo) {
-			var ms = new RegexTextUtils();
-			foreach (var page in pagesTodo.GetConsumingEnumerable())
-				ProcessPage(entriesTodo, page, ms);
-		}
-
-		static void ProcessPage(BlockingCollection<Entry[]> entriesTodo, XElement page, RegexTextUtils ms) {
-			string pagetext = ms.StripWikiMarkupMarkup(WikiXmlReader.GetArticleText(page));
-			string pageWSnormal = WhitespaceNormalizer.Normalize(pagetext);
-
-			Entry[] entries = (
-				from sentence in ms.FindEnglishSentences(pageWSnormal)
-				where ms.GradeEnglishSentence(sentence) > 2.5
-				from entry in ms.ExtractWordsPrecededByAOrAn(sentence)
-				select entry).ToArray();
-			entriesTodo.Add(entries);
-		}
-	}
+            Task.Factory.StartNew(() => {
+                using (var stream = wikiPath.OpenRead())
+                using (var reader = XmlReader.Create(stream))
+                    while (reader.Read())
+                        if (reader.NodeType == XmlNodeType.Element && reader.LocalName == "page")
+                            pagesTodo.Add((XElement)XNode.ReadFrom(reader));
+                pagesTodo.CompleteAdding();
+            }, TaskCreationOptions.LongRunning);
+            return pagesTodo;
+        }
+    }
 }
---
+++ WikipediaAvsAnTrieExtractor/RegexTextUtils.FindAvsAnSightings.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+using System.Linq;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace AvsAnTrie {
+    public partial class RegexTextUtils {
+        public AvsAnSighting[] FindAvsAnSightings(XElement page) {
+            string pagetext = StripWikiMarkupMarkup(WikiXmlReader.GetArticleText(page));
+            string pageWSnormal = WhitespaceNormalizer.Normalize(pagetext);
+
+            return (
+                from sentence in FindEnglishSentences(pageWSnormal)
+                where GradeEnglishSentence(sentence) > 2.5
+                from entry in ExtractWordsPrecededByAOrAn(sentence)
+                select entry).ToArray();
+        }
+    }
+}
