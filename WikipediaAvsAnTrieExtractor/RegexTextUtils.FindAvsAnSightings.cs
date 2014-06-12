213e2edd2516c7b0f6a808b6fe970a0f85829ec6
Eamon Nerbonne <eamon@nerbonne.org>
2014-06-12 19:17:07 +0200

    To allow better profiling, use foreach, not linq

--- WikipediaAvsAnTrieExtractor/RegexTextUtils.FindAvsAnSightings.cs
+++ WikipediaAvsAnTrieExtractor/RegexTextUtils.FindAvsAnSightings.cs
@@ -8,12 +8,17 @@ namespace AvsAnTrie {
         public AvsAnSighting[] FindAvsAnSightings(XElement page)
         {
             var pageTextContent = GetPagePlainTextContent(page);
-
-            return (
-                from sentence in FindEnglishSentences(pageTextContent)
-                where GradeEnglishSentence(sentence) > 2.5
-                from entry in ExtractWordsPrecededByAOrAn(sentence)
-                select entry).ToArray();
+            var retval=  new List<AvsAnSighting>();
+            foreach (var sentence in FindEnglishSentences(pageTextContent))
+                if(GradeEnglishSentence(sentence) > 2.5)
+                    foreach (var entry in ExtractWordsPrecededByAOrAn(sentence))
+                        retval.Add(entry);
+            return retval.ToArray();
+            //return (
+            //    from sentence in FindEnglishSentences(pageTextContent)
+            //    where GradeEnglishSentence(sentence) > 2.5
+            //    from entry in ExtractWordsPrecededByAOrAn(sentence)
+            //    select entry).ToArray();
         }
 
         string GetPagePlainTextContent(XElement page)
