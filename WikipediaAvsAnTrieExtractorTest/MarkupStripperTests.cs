1444cb0ba3434bf5a9dec5518c3df4323527aa3f
Eamon Nerbonne <eamon@nerbonne.org>
2014-06-16 14:12:19 +0200

    Add wiki markup stripping test scaffold

---
+++ WikipediaAvsAnTrieExtractorTest/MarkupStripperTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpressionToCodeLib;
+using WikipediaAvsAnTrieExtractor;
+using Xunit;
+
+namespace WikipediaAvsAnTrieExtractorTest {
+    
+    public class MarkupStripperTests {
+        readonly RegexTextUtils utils = new RegexTextUtils();
+
+        [Fact]
+        public void StripsMarkup()
+        {
+            var data = new Dictionary<string, string>
+            {
+            };
+            foreach (var kvp in data) {
+                PAssert.That(() => kvp.Value == utils.StripWikiMarkup(kvp.Key));
+            }
+        }
+    }
+}
