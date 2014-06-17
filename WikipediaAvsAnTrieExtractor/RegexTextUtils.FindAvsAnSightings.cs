0af630781103f59a390d60898e399e9a2b9244fe
Eamon Nerbonne <eamon@nerbonne.org>
2014-06-17 20:59:02 +0200

    Implement new english sentence grader (faster, prefers longer sentences slightly)
    --HG--
    extra : amend_source : 76fe05b8dc8ebc7eb4d216aeb5a7091f39a6779b

--- WikipediaAvsAnTrieExtractor/RegexTextUtils.FindAvsAnSightings.cs
+++ WikipediaAvsAnTrieExtractor/RegexTextUtils.FindAvsAnSightings.cs
@@ -3,19 +3,17 @@ using System;
 using System.Collections.Generic;
 
 namespace WikipediaAvsAnTrieExtractor {
-    public partial class RegexTextUtils
-    {
-        public AvsAnSighting[] FindAvsAnSightings(string page)
-        {
+    public partial class RegexTextUtils {
+        public AvsAnSighting[] FindAvsAnSightings(string page) {
             var strippedPage = StripWikiMarkup(page);
             var plaintextPage = WhitespaceNormalizer.Normalize(strippedPage);
-            var retval=  new List<AvsAnSighting>();
+            var retval = new List<AvsAnSighting>();
 
-            foreach (var sentence in FindEnglishSentences(plaintextPage))
-            {
-                if (GradeEnglishSentence(sentence) > 2.5)
+            foreach (var sentence in FindEnglishSentences(plaintextPage)) {
+                if (GradeEnglishSentence(sentence) > 1.0) {
                     foreach (var entry in ExtractWordsPrecededByAOrAn(sentence))
                         retval.Add(entry);
+                }
             }
             return retval.ToArray();
         }
--- WikipediaAvsAnTrieExtractor/RegexTextUtils.GradeEnglishSentence.cs
+++ WikipediaAvsAnTrieExtractor/RegexTextUtils.GradeEnglishSentence.cs
@@ -1,48 +1,87 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Text;
 using System.Linq;
 using System;
 using System.Collections.Generic;
 
 namespace WikipediaAvsAnTrieExtractor {
     public partial class RegexTextUtils {
-
-        readonly Regex words = new Regex(@"(?<=(^|\s)[^\w\s]*)\w+", RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.CultureInvariant);
-        readonly Regex capwordsAfterFirst = new Regex(@" [^\w\s]*[A-Z]\S*", RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.CultureInvariant);
+        private static readonly bool[] isSeparatorChar =
+            Enumerable.Range(0, 65536)
+                .Select(c => CharUnicodeInfo.GetUnicodeCategory((char)c))
+                .Select(cat =>
+                    cat == UnicodeCategory.Control ||
+                    cat >= UnicodeCategory.SpaceSeparator && cat <= UnicodeCategory.ParagraphSeparator
+                    || cat >= UnicodeCategory.ConnectorPunctuation && cat <= UnicodeCategory.OtherPunctuation
+                ).ToArray();
 
         public double GradeEnglishSentence(string sentenceCandidate) {
-            int capCount = sentenceCandidate.Count(c => c >= 'A' && c <= 'Z');
-            int numCount = sentenceCandidate.Count(c => c >= '0' && c <= '9');//CharUnicodeInfo.GetUnicodeCategory(capCount) 
-            int wordCharCount = capCount + sentenceCandidate.Count(c => c >= 'a' && c <= 'z');
-            var wordMatches = words.Matches(sentenceCandidate);
-            int wordCount = wordMatches.Count;
-
-            double pref = 0.0;
-            if (dictionary != null) {
-                int inDictCount = 0;
-                foreach (Match m in wordMatches) {
-                    var word = m.Value;
-                    int ignore;
-                    inDictCount +=
-                        dictionary.Contains(m.Value) ? 2
-                        : int.TryParse(m.Value, out ignore) ? 1 //numbers aren't quite valid words in the dictionary, but they're not nonsense either.
-                        : word[0] >= 'A' && word[0] <= 'Z' ? 1 //don't quite expect proper nouns to be in the dictionary.
-                        : 0;
-                }
-                pref = (inDictCount - wordCount) / (double)wordCount;
-            }
-            int capWordCount = capwordsAfterFirst.Matches(sentenceCandidate).Count;
             int charCount = sentenceCandidate.Length;
+            int capCount = 0, numCount = 0, wordCharCount = 0;
+            int inDictScore = 0;
+            int capWordCount = 0, wordCount = 0, spaceCount = 0;
+            bool seenWord = false;
+            bool inWord = false;
+            int wordStart = 0;
+            foreach (var c in sentenceCandidate) {
+                if (c >= 'A' && c <= 'Z')
+                    capCount++;
+                else if (c >= '0' && c <= '9')
+                    numCount++;
+                else if (c >= 'a' && c <= 'z')
+                    wordCharCount++;
+                else if (c == ' ')
+                    spaceCount++;
+            }
+
+            for (int i = 0; i <= sentenceCandidate.Length; i++) {
+                var shouldBeInWord = i < sentenceCandidate.Length
+                    && (!isSeparatorChar[sentenceCandidate[i]]
+                    || inWord && sentenceCandidate[i] == '\'');
+                if (inWord == shouldBeInWord) continue;
+                if (!inWord) {
+                    inWord = true;
+                    wordStart = i;
+                } else {
+                    inWord = false;
+                    var word = sentenceCandidate.Substring(wordStart, i - wordStart);
+                    int ignore;
+                    inDictScore +=
+                        dictionary.Contains(word)
+                            ? 2
+                            : int.TryParse(word, out ignore)
+                                ? 1
+                        //numbers aren't quite valid words in the dictionary, but they're not nonsense either.
+                                : word[0] >= 'A' && word[0] <= 'Z'
+                                    ? 1 //don't quite expect proper nouns to be in the dictionary.
+                                    : 0;
+                    wordCount++;
+                    if (seenWord) {
+                        if (word[0] >= 'A' && word[0] <= 'Z')
+                            capWordCount++;
+                    } else
+                        seenWord = true;
+                }
+            }
+            wordCharCount += capCount;
+
+            double pref = (inDictScore - wordCount) / (double)wordCount;
 
             double lineCost = sentenceCandidate.Length == 0 || sentenceCandidate[sentenceCandidate.Length - 1] == '\n' ? -0.4 : 0.0; //if they don't end with punctuation... hmmm.
 
             double capRate = wordCount == 1 ? 0.5 :
                 capWordCount / (double)(wordCount - 1);
 
-            double grade = (wordCharCount - numCount - capCount) / (double)charCount
+            double grade = (
+                (wordCharCount - numCount - capCount) / (double)charCount
                            + 0.3 * Math.Min(wordCount - capWordCount, 6)
                            - 0.3 * capRate
                            + pref
-                           + lineCost;
+                           + lineCost
+                           + 0.75 * Math.Tanh(0.5*(spaceCount - 3.0)) 
+                           ) / 3.0;
             return grade;
         }
     }
