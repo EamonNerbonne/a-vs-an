c5a2dedc442fd522877efe9b447db4326624fddf
Eamon Nerbonne <eamon@nerbonne.org>
2014-06-18 16:18:37 +0200

    Allow capital A/An too.

--- WikipediaAvsAnTrieExtractor/RegexTextUtils.ExtractWordsAfterAOrAn.cs
+++ WikipediaAvsAnTrieExtractor/RegexTextUtils.ExtractWordsAfterAOrAn.cs
@@ -8,7 +8,7 @@ namespace WikipediaAvsAnTrieExtractor {
         //Note: regexes are NOT static and shared because of... http://stackoverflow.com/questions/7585087/multithreaded-use-of-regex
         //This code is bottlenecked by regexes, so this really matters, here.
 
-        readonly Regex followingAn = new Regex(@"\b(?<article>a|an) [\(""'“‘-]?(?<word>\S+)", RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.CultureInvariant);
+        readonly Regex followingAn = new Regex(@"\b(?<article>[Aa]n?) [\(""'“‘-]*(?<word>\S+)", RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.CultureInvariant);
         public IEnumerable<AvsAnSighting> ExtractWordsPrecededByAOrAn(string text) {
             return
                 from Match m in followingAn.Matches(text)
