1a21784512a260c77c54d8b87ced22a7da25c4ff
Eamon Nerbonne <eamon@nerbonne.org>
2014-06-19 14:47:03 +0200

    Whoops, forgot namespace import

--- WikipediaAvsAnTrieExtractor/Program.cs
+++ WikipediaAvsAnTrieExtractor/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Concurrent;
 using System.Threading;
