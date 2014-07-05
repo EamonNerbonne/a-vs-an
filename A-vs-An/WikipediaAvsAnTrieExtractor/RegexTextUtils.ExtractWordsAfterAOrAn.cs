6e573d6c1800c3d5ccc388c9dc34690724a85367
Eamon Nerbonne <eamon@nerbonne.org>
2014-07-05 15:46:22 +0200

    Single quotes are parts of words like Xi'an & Chang'an.

--- A-vs-An/WikipediaAvsAnTrieExtractor/RegexTextUtils.ExtractWordsAfterAOrAn.cs
+++ A-vs-An/WikipediaAvsAnTrieExtractor/RegexTextUtils.ExtractWordsAfterAOrAn.cs
@@ -8,9 +8,9 @@ namespace WikipediaAvsAnTrieExtractor {
         //Note: regexes are NOT static and shared because of... http://stackoverflow.com/questions/7585087/multithreaded-use-of-regex
         //This code is bottlenecked by regexes, so this really matters, here.
 
-        readonly Regex followingAn = new Regex(@"(^(?<article>An?)|[\s""()‘’“”'](?<article>an?)) [""()‘’“”$']*(?<word>[^\s""()‘’“”$'-]+)", RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.CultureInvariant);
+        readonly Regex followingAn = new Regex(@"(^(?<article>An?)|[\s""()‘’“”](?<article>an?)) [""()‘’“”$']*(?<word>[^\s""()‘’“”$-]+)", RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.CultureInvariant);
         //watch out for dashes before "A" because of things like "Triple-A annotation"
-
+        //Becareful of words like Chang'an - ' is not a seperator here.
         public IEnumerable<AvsAnSighting> ExtractWordsPrecededByAOrAn(string text) {
             return
                 from Match m in followingAn.Matches(text)
