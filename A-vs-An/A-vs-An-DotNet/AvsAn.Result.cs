28fccaebb0e51d1a10cc26fd5f491dd833880b34
Eamon Nerbonne <eamon@nerbonne.org>
2014-07-08 07:42:13 +0200

    Split out semi-private parts into an internals directory to avoid namespace pollution.
    Split large AvsAn class into multiple files
    
    --HG--
    rename : A-vs-An/A-vs-An-DotNet/MutableNode.cs => A-vs-An/A-vs-An-DotNet/Internals/MutableNode.cs
    rename : A-vs-An/A-vs-An-DotNet/Node.cs => A-vs-An/A-vs-An-DotNet/Internals/Node.cs
    rename : A-vs-An/A-vs-An-DotNet/Ratio.cs => A-vs-An/A-vs-An-DotNet/Internals/Ratio.cs

---
+++ A-vs-An/A-vs-An-DotNet/AvsAn.Result.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvsAnLib {
+    public partial class AvsAn {
+        // ReSharper disable MemberCanBePrivate.Global
+        //part of public nuget api, usages may be in external code.
+        public struct Result {
+            /// <summary>
+            /// How often this prefix was preceded by "a" on wikipedia.
+            /// </summary>
+            public readonly int aCount;
+            /// <summary>
+            /// How often this prefix was preceded by "an" on wikipedia.
+            /// </summary>
+            public readonly int anCount;
+            /// <summary>
+            /// The prefix of the word on which the determination was based.
+            /// </summary>
+            public string Prefix {
+                get {
+                    return Depth > Word.Length
+                        ? Word + new string(' ', Depth - Word.Length)
+                        : Word.Substring(0, Depth);
+                }
+            }
+            /// <summary>
+            /// The tested word.
+            /// </summary>
+            public readonly string Word;
+            /// <summary>
+            /// How many letters of the tested word were used in determining the appropriate article.
+            /// </summary>
+            public readonly int Depth;
+            /// <summary>
+            /// The article you should use.
+            /// </summary>
+            public string Article { get { return aCount >= anCount ? "a" : "an"; } }
+            public Result(int aCount, int anCount, string word, int depth) {
+                this.aCount = aCount;
+                this.anCount = anCount;
+                Word = word;
+                Depth = depth;
+            }
+        }
+        // ReSharper restore MemberCanBePrivate.Global
+    }
+}
---
+++ A-vs-An/A-vs-An-DotNet/Internals/WordQuery.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvsAnLib.Internals {
+    static class WordQuery {
+        public static AvsAn.Result Query(Node node, string word, int depth) {
+            Ratio result = node.ratio;
+            while (true)
+                if (depth >= word.Length) return new AvsAn.Result(result.aCount, result.anCount, word, depth);
+                else if (word[depth] == '(' || word[depth] == '\'' || word[depth] == '"') depth++;
+                else break;
+            while (true) {
+                if (node.SortedKids == null) break;
+                char c = depth == word.Length ? ' ' : word[depth];
+                int start = 0, end = node.SortedKids.Length;
+                while (end - start > 1) {
+                    int midpoint = end + start >> 1;
+                    if (node.SortedKids[midpoint].c <= c)
+                        start = midpoint;
+                    else
+                        end = midpoint;
+                }
+                if (node.SortedKids[start].c == c) {
+                    node = node.SortedKids[start];
+                    depth++;
+                    if (node.ratio.isSet)
+                        result = node.ratio;
+                } else
+                    break;
+
+                if (depth > word.Length)
+                    break;
+            }
+            return new AvsAn.Result(result.aCount, result.anCount, word, depth);
+        }
+
+    }
+}
