db93e41e06c8b9c9851c03fe5c63a59b8f7fff78
Eamon Nerbonne <eamon@nerbonne.org>
2014-07-19 13:45:49 +0200

    Update markup stripping tests to reflect new practice.
     Always nice to see em work - caught the closing tag issue due to the test!

--- A-vs-An/WikipediaAvsAnTrieExtractorTest/MarkupStripperTests.cs
+++ A-vs-An/WikipediaAvsAnTrieExtractorTest/MarkupStripperTests.cs
@@ -12,58 +12,6 @@ namespace WikipediaAvsAnTrieExtractorTest {
         readonly RegexTextUtils utils = new RegexTextUtils();
 
         [Fact]
-        public void StripsRedirects() {
-            Assert.Equal("", utils.StripWikiMarkup(@"#REDIRECT [[Foreign relations of the Czech Republic]]"));
-        }
-
-        [Fact]
-        public void StripsHtml() {
-            Assert.Equal("test", utils.StripWikiMarkup(@"<b>test</b>"));
-        }
-        [Fact]
-        public void StripsMathEntirelyHtml() {
-            Assert.Equal("atest", utils.StripWikiMarkup(@"a<math><nested></x></math>test"));
-        }
-        [Fact]
-        public void StripsCommentsEntirelyHtml() {
-            Assert.Equal("atest", utils.StripWikiMarkup(@"a<!--<math>x</math>y--z-->test"));
-        }
-
-        [Fact]
-        public void ReplacesNormalWikiLinks() {
-            Assert.Equal("TestLink", utils.StripWikiMarkup(@"[[TestLink]]"));
-        }
-
-        [Fact]
-        public void ReplacesWikiLinksWithColons() {
-            Assert.Equal("WP:Policy", utils.StripWikiMarkup(@"[[WP:Policy]]"));
-        }
-
-        [Fact]
-        public void ReplacesWikiLinksHavingText() {
-            Assert.Equal("Some Text", utils.StripWikiMarkup(@"[[WP:Policy|Some Text]]"));
-        }
-
-        [Fact]
-        public void StripsLanguageLinks() {
-            Assert.Equal("", utils.StripWikiMarkup(@"[[en:English Link]]"));
-        }
-        [Fact]
-        public void StripsCategoryinks() {
-            Assert.Equal("", utils.StripWikiMarkup(@"[[Category:Some Category]]"));
-        }
-
-        [Fact] 
-        public void SupportsExternalLinksHavingText() {
-            Assert.Equal("an example", utils.StripWikiMarkup(@"[http://example.org an example]"));
-        }
-
-        [Fact]
-        public void RetainsNonLinkSquareBrackets() {
-            Assert.Equal("[Q]", utils.StripWikiMarkup(@"[Q]"));
-        }
-
-        [Fact]
         public void Wikipage_AaRiver() {
             Approvals.Verify(utils.StripWikiMarkup(@"'''Aa River''' may refer to:
 
---
+++ A-vs-An/WikipediaAvsAnTrieExtractorTest/MarkupStripperUnitTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WikipediaAvsAnTrieExtractor;
+using Xunit;
+
+namespace WikipediaAvsAnTrieExtractorTest {
+    public class MarkupStripperUnitTests {
+        readonly RegexTextUtils utils = new RegexTextUtils();
+
+        [Fact]
+        public void StripsRedirects() {
+            Assert.Equal("", utils.StripWikiMarkup(@"#REDIRECT [[Foreign relations of the Czech Republic]]"));
+        }
+
+        [Fact]
+        public void StripsHtml() {
+            Assert.Equal("test", utils.StripWikiMarkup(@"<b>test</b>"));
+        }
+        [Fact]
+        public void StripsMathEntirelyHtml() {
+            Assert.Equal("atest", utils.StripWikiMarkup(@"a<math><nested></x></math>test"));
+        }
+        [Fact]
+        public void StripsCommentsEntirelyHtml() {
+            Assert.Equal("atest", utils.StripWikiMarkup(@"a<!--<math>x</math>y--z-->test"));
+        }
+
+        [Fact]
+        public void ReplacesNormalWikiLinks() {
+            Assert.Equal("TestLink", utils.StripWikiMarkup(@"[[TestLink]]"));
+        }
+
+        [Fact]
+        public void ReplacesWikiLinksWithColons() {
+            Assert.Equal("WP:Policy", utils.StripWikiMarkup(@"[[WP:Policy]]"));
+        }
+
+        [Fact]
+        public void ReplacesWikiLinksHavingText() {
+            Assert.Equal("Some Text", utils.StripWikiMarkup(@"[[WP:Policy|Some Text]]"));
+        }
+
+        [Fact]
+        public void StripsLanguageLinks() {
+            Assert.Equal("", utils.StripWikiMarkup(@"[[en:English Link]]"));
+        }
+        [Fact]
+        public void StripsCategoryinks() {
+            Assert.Equal("", utils.StripWikiMarkup(@"[[Category:Some Category]]"));
+        }
+
+        [Fact]
+        public void SupportsExternalLinksHavingText() {
+            Assert.Equal("an example", utils.StripWikiMarkup(@"[http://example.org an example]"));
+        }
+
+        [Fact]
+        public void RetainsNonLinkSquareBrackets() {
+            Assert.Equal("[Q]", utils.StripWikiMarkup(@"[Q]"));
+        }
+
+
+    }
+}
