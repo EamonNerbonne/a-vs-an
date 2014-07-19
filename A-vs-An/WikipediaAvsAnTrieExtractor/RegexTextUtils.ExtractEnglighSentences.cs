245caeb369a755cd228534fcd08bf676d8c90409
Eamon Nerbonne <eamon@nerbonne.org>
2014-07-19 11:28:09 +0200

    Delete dead code.

--- A-vs-An/WikipediaAvsAnTrieExtractor/RegexTextUtils.ExtractEnglighSentences.cs
+++ A-vs-An/WikipediaAvsAnTrieExtractor/RegexTextUtils.ExtractEnglighSentences.cs
@@ -41,43 +41,8 @@ namespace WikipediaAvsAnTrieExtractor {
 (?=\s|$)
 ";
         readonly Regex sentenceFinderRegex = new Regex(sentenceRegex, options);
-        //readonly Regex oldSentenceFinderRegex = new Regex(@"(?<=[\.\?!]\s+|^)((?<sentence>(\(|" + "\"" + @")?[A-Z]( ([Ss]t|Mrs?|dr|ed|c|v(s|ol)?|[nN]o(?=\s+[0-9])|et al)\.|\(\w+\.|[A-Z]\. |\.([\w\d]| (\w\.( \w\.)*|[a-z]))|[^\.\n\?!])+[\.\?!\n](\)|" + "\"" + @")?))(?=\s|$)", options);
-        
-//        readonly Regex oldSentenceFinderRegex = new Regex(@"
-//            (?<=[.?!]\s+|^)
-//                (?<sentence>
-//                    [\(""]?
-//                    (?=[A-Z])
-//                    (
-//                        (\s|^)(
-//                            \(\w+
-//                            |c
-//                            |[dD]r
-//                            |e(t[ ]al|d|\.g)
-//                            |Gov
-//                            |i\.e
-//							|Lt
-//                            |M(rs?|t)
-//                            |[nN]o(?=\s+[0-9])
-//                            |[Ss]t
-//                            |[vV](s|ol)?
-//                            |[A-Z](\.[A-Z])*
-//                        )\.
-//                        |\.[\w\d]
-//                        |[^\.\n\?!]
-//                    )+
-//                    [.?!\n]
-//                    [)""]*
-//                )
-//            (?=\s|$)", options | RegexOptions.IgnorePatternWhitespace);
 
-        //public IEnumerable<string> FindEnglishSentencesOld(string text) {
-        //    foreach (Match m in oldSentenceFinderRegex.Matches(text))
-        //        yield return m.Groups["sentence"].Value;
-        //}
-
-        public IEnumerable<string> FindEnglishSentences(string text)
-        {
+        public IEnumerable<string> FindEnglishSentences(string text) {
             foreach (Match m in sentenceFinderRegex.Matches(text))
                 yield return m.Value;
         }
