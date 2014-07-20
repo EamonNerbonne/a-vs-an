6c8bbbc69345cb0a7c7d6cd5c22eed461c9d23f0
Eamon Nerbonne <eamon@nerbonne.org>
2014-07-20 12:46:53 +0200

    (minor) cleanup.

--- A-vs-An/WikipediaAvsAnTrieExtractor/IncrementPrefixExtensions.cs
+++ A-vs-An/WikipediaAvsAnTrieExtractor/IncrementPrefixExtensions.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using AvsAnLib.Internals;
 
 namespace WikipediaAvsAnTrieExtractor {
