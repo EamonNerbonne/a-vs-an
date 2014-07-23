320644248c864a77c1815166799c2ad633603697
Eamon Nerbonne <eamon@nerbonne.org>
2014-07-23 20:25:35 +0200

    Allow conversion to mutable too.

---
+++ A-vs-An/WikipediaAvsAnTrieExtractor/ToMutableExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AvsAnLib.Internals;
+
+namespace WikipediaAvsAnTrieExtractor {
+    public static class ToMutableExtension {
+        public static MutableNode ToMutableNode(this Node node) {
+            return new MutableNode {
+                ratio = node.ratio,
+                Kids = node.SortedKids == null ? null :
+                    node.SortedKids.ToDictionary(n => n.c, ToMutableNode)
+            };
+        }
+    }
+}
