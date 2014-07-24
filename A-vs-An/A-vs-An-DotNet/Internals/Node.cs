fc45ea7eaedf29462fa94d3412dba82c3b28038f
Eamon Nerbonne <eamon@nerbonne.org>
2014-07-24 23:44:01 +0200

    Implement direct-to-node deserialization (faster despite nasty array issues)

--- A-vs-An/A-vs-An-DotNet/Internals/Node.cs
+++ A-vs-An/A-vs-An-DotNet/Internals/Node.cs
@@ -23,5 +23,53 @@ namespace AvsAnLib.Internals {
         }
 
 
+        public void LoadPrefixRatio(string prefix, int depth, Ratio prefixRatio) {
+            if (prefix.Length == depth) {
+                ratio = prefixRatio;
+            } else {
+                int idx;
+                char kidC = prefix[depth];
+                if (SortedKids == null) {
+                    SortedKids = new[] { new Node { c = kidC, } };
+                    idx = 0;
+                } else {
+                    var nearestIdx = FindNearestNode(kidC);
+                    idx = nearestIdx;
+                    if (SortedKids[nearestIdx].c != kidC) {
+                        if (SortedKids[nearestIdx].c < kidC) {
+                            idx = nearestIdx + 1;
+                        }
+                        InsertBefore(idx, kidC);
+                    }
+                }
+                SortedKids[idx].LoadPrefixRatio(prefix, depth + 1, prefixRatio);
+            }
+        }
+
+        int FindNearestNode(char c) {
+            int start = 0, end = SortedKids.Length;
+            while (end - start > 1) {
+                int midpoint = end + start >> 1;
+                if (SortedKids[midpoint].c <= c) {
+                    start = midpoint;
+                } else {
+                    end = midpoint;
+                }
+            }
+            return start;
+        }
+
+        void InsertBefore(int idx, char c) {
+            var newArr = new Node[SortedKids.Length + 1];
+            int i = 0;
+            for (; i < idx; i++) {
+                newArr[i] = SortedKids[i];
+            }
+            newArr[idx].c = c;
+            for (; i < SortedKids.Length; i++) {
+                newArr[i + 1] = SortedKids[i];
+            }
+            SortedKids = newArr;
+        }
     }
 }
