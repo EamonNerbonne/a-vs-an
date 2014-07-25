e7921a3bffc9b9c1e9a14f920bd44517009b5c1d
Eamon Nerbonne <eamon@nerbonne.org>
2014-07-25 11:43:58 +0200

    Get rid of old entity decoder

--- A-vs-An/WikipediaAvsAnTrieExtractor/RegexTextUtils.StripWikiMarkup.cs
+++ A-vs-An/WikipediaAvsAnTrieExtractor/RegexTextUtils.StripWikiMarkup.cs
@@ -19,18 +19,6 @@ namespace WikipediaAvsAnTrieExtractor {
         }
 
         public string DecodeEntities(string text) {
-            return decodeEntitiesRegex.Replace(text, m => {
-                char c;
-                var key = text.Substring(m.Index + 1, m.Length - 2);
-                if (HtmlEntities.EntityLookup.TryGetValue(key, out c)) {
-                    return c.ToString();
-                } else {
-                    return m.Value;
-                }
-            });
-        }
-
-        public string DecodeEntities2(string text) {
             var sb = new StringBuilder();
             int pos = 0;
             while (true) {
@@ -106,8 +94,6 @@ namespace WikipediaAvsAnTrieExtractor {
 
             return sb.ToString();
         }
-
-        readonly Regex decodeEntitiesRegex = new Regex(@"&[A-Za-z0-9]+;", options);
 
         readonly Regex markupToReplaceWithQuotes = new Regex(@"
     ''+ #For a vs. an: important that emphasis isn't around article
--- A-vs-An/WikipediaAvsAnTrieExtractorTest/EntityDecoderTest.cs
+++ A-vs-An/WikipediaAvsAnTrieExtractorTest/EntityDecoderTest.cs
@@ -39,35 +39,5 @@ namespace WikipediaAvsAnTrieExtractorTest {
         public void ReplacesInTheMiddleOfText() {
             Assert.Equal("The html &amp; entity is an &.", utils.DecodeEntities("The html &amp;amp; entity is an &."));
         }
-
-        [Fact]
-        public void AmpLessTextUnchanged2() {
-            Assert.Equal("This is text.", utils.DecodeEntities2("This is text."));
-        }
-
-        [Fact]
-        public void SpacedAmpUnchanged2() {
-            Assert.Equal("This is a Q & A.", utils.DecodeEntities2("This is a Q & A."));
-        }
-        [Fact]
-        public void NonSemicolonAmpUnchanged2() {
-            Assert.Equal("This is a Q&A.", utils.DecodeEntities2("This is a Q&A."));
-        }
-        [Fact]
-        public void TrivialLtEntityReplaced2() {
-            Assert.Equal("<", utils.DecodeEntities2("&lt;"));
-        }
-        [Fact]
-        public void MessyLtEntityReplaced2() {
-            Assert.Equal("&<;", utils.DecodeEntities2("&&lt;;"));
-        }
-        [Fact]
-        public void UnknownEntityUnchanged2() {
-            Assert.Equal("&xyz;", utils.DecodeEntities2("&xyz;"));
-        }
-        [Fact]
-        public void ReplacesInTheMiddleOfText2() {
-            Assert.Equal("The html &amp; entity is an &.", utils.DecodeEntities2("The html &amp;amp; entity is an &."));
-        }
     }
 }
