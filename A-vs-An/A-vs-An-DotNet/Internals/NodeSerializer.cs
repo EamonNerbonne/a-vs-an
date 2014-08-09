cb42e706ea12de272b223632cae78fedb6e69193
Eamon Nerbonne <eamon@nerbonne.org>
2014-08-09 15:43:50 +0200

    Replace regex based parser with handrolled (faster) implementation.

--- A-vs-An/A-vs-An-DotNet/Internals/NodeSerializer.cs
+++ A-vs-An/A-vs-An-DotNet/Internals/NodeSerializer.cs
@@ -1,27 +1,72 @@
-using System;
-using System.Globalization;
 using System.Text;
-using System.Text.RegularExpressions;
 
 namespace AvsAnLib.Internals {
     public static class NodeSerializer {
         public static Node Deserialize(string rawDict) {
             var mutableRoot = new Node();
-            foreach (
-                Match m in Regex.Matches(rawDict, @"([^(\n]*)\(([0-9a-f]*):([0-9a-f]*)\)", RegexOptions.CultureInvariant)
-                )
+            int pos = 0;
+            int len = rawDict.Length;
+            while (true) {
+                while (true) {
+                    if (pos >= len)
+                        return mutableRoot;
+                    if (rawDict[pos] != ' ' && rawDict[pos] != '\n' && rawDict[pos] != '\r')
+                        break;
+                    pos++;
+                }
+                int start = pos;
+                while (true) {
+                    if (pos >= len)
+                        return mutableRoot;
+                    if (rawDict[pos] == '(')
+                        break;
+                    pos++;
+                }
+                int paren0 = pos;
+                pos++;
+                while (true) {
+                    if (pos >= len)
+                        return mutableRoot;
+                    if (rawDict[pos] == ':')
+                        break;
+                    pos++;
+                }
+                int sep = pos;
+                while (true) {
+                    if (pos >= len)
+                        return mutableRoot;
+                    if (rawDict[pos] == ')')
+                        break;
+                    pos++;
+                }
+                int paren1 = pos;
                 mutableRoot.LoadPrefixRatio(
-                    m.Groups[1].Value,
-                    0,
-                    new Ratio {
-                        aCount = parseHex(m.Groups[2].Value),
-                        anCount = parseHex(m.Groups[3].Value)
-                    });
-            return mutableRoot;
+                   rawDict.Substring(start, paren0 - start),
+                   0,
+                   new Ratio {
+                       aCount = parseHex(rawDict, paren0 + 1, sep),
+                       anCount = parseHex(rawDict, sep + 1, paren1)
+                   });
+                pos++;
+            }
         }
 
-        static int parseHex(string str) {
-            return str == "" ? 0 : Int32.Parse(str, NumberStyles.AllowHexSpecifier, CultureInfo.InvariantCulture);
+        static int parseHex(string str, int start, int end) {
+            int retval = 0;
+            while (start < end) {
+                retval = retval << 4;
+                char c = str[start];
+                int charVal;
+                if (c <= '9')
+                    charVal = c - '0';
+                else if (c <= 'F')
+                    charVal = c - 'A' + 10;
+                else
+                    charVal = c - 'a' + 10;
+                retval += charVal;
+                start++;
+            }
+            return retval;
         }
 
         public static string SerializeReadably(Node node) {
