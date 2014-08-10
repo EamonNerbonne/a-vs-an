32b2db245c99a78ef35aee71831c426f4271fbea
Eamon Nerbonne <eamon@nerbonne.org>
2014-08-10 11:42:09 +0200

    Improve performance of WordQuery.

--- A-vs-An/A-vs-An-DotNet/AvsAn.cs
+++ A-vs-An/A-vs-An-DotNet/AvsAn.cs
@@ -11,20 +11,34 @@ namespace AvsAnLib {
         /// <param name="word">The word to test.  AvsAn assumes this is a complete word; in some cases word-prefixes may result in a differing classification that complete words.  If you wish to classify an incomplete word (a prefix), append a non-word, non-space character such as the underscore "_" as a placeholder for further letters.</param>
         /// <returns>A classification result indicating "a" or "an" with some wikipedia-derived statistics.</returns>
         public static Result Query(string word) {
-            return WordQuery.Query(BuiltInDictionary.Root, word, 0);
+            return WordQuery.Query(BuiltInDictionary.Root, word);
         }
 
         // ReSharper disable MemberCanBePrivate.Global
         //part of public nuget api, usages may be in external code.
         public struct Result {
+            readonly Ratio ratio;
+
             /// <summary>
             /// How often this prefix was preceded by "a" on wikipedia.
             /// </summary>
-            public readonly int aCount;
+            public int aCount { get { return ratio.aCount; } }
+
             /// <summary>
             /// How often this prefix was preceded by "an" on wikipedia.
             /// </summary>
-            public readonly int anCount;
+            public int anCount { get { return ratio.anCount; } }
+
+            /// <summary>
+            /// How often this prefix occurred on wikipedia.  Equivalent to aCount+anCount
+            /// </summary>
+            public int Occurence { get { return ratio.Occurence; } }
+
+            /// <summary>
+            /// How many more "a"s occurred before this prefix than "an"s.
+            /// </summary>
+            public int AminAnDiff { get { return ratio.AminAnDiff; } }
+
             /// <summary>
             /// The prefix of the word on which the determination was based.
             /// </summary>
@@ -46,12 +60,11 @@ namespace AvsAnLib {
             /// <summary>
             /// The article you should use.
             /// </summary>
-            public string Article { get { return aCount >= anCount ? "a" : "an"; } }
-            public Result(int aCount, int anCount, string word, int depth) {
-                this.aCount = aCount;
-                this.anCount = anCount;
+            public string Article { get { return ratio.AminAnDiff >= 0 ? "a" : "an"; } }
+            public Result(Ratio ratio, string word, int depth) {
                 Word = word;
                 Depth = depth;
+                this.ratio = ratio;
             }
         }
 
@@ -60,7 +73,7 @@ namespace AvsAnLib {
         }
 
 
-        public enum IndefiniteArticle { A=1, An=-1 }
+        public enum IndefiniteArticle { A = 1, An = -1 }
 
         //part of public nuget api, usages may be in external code.
         public struct PlainResult {
--- A-vs-An/A-vs-An-DotNet/Internals/WordQuery.cs
+++ A-vs-An/A-vs-An-DotNet/Internals/WordQuery.cs
@@ -5,68 +5,103 @@
         /// </summary>
         /// <param name="node">The root of the (remaining) prefix-trie to check</param>
         /// <param name="word">The word being checked</param>
-        /// <param name="depth">The depth within the word/trie (initially 0).</param>
-        public static AvsAn.Result Query(Node node, string word, int depth) {
+        public static AvsAn.Result Query(Node node, string word) {
+            var depth = 0;
             Ratio result = node.ratio;
-            while (true)
-                if (depth >= word.Length) return new AvsAn.Result(result.aCount, result.anCount, word, depth);
-                else if (word[depth] == '(' || word[depth] == '\'' || word[depth] == '"') depth++;
-                else break;
+            int length = word.Length;
             while (true) {
-                if (node.SortedKids == null) break;
-                char c = depth == word.Length ? ' ' : word[depth];
-                int start = 0, end = node.SortedKids.Length;
-                while (end - start > 1) {
-                    int midpoint = end + start >> 1;
-                    if (node.SortedKids[midpoint].c <= c)
-                        start = midpoint;
-                    else
-                        end = midpoint;
+                if (depth >= length) {
+                    return new AvsAn.Result(result, word, depth);
+                } else {
+                    char c = word[depth];
+                    if (c == '"' || c == '‘' || c == '’' || c == '“' || c == '”' || c == '$' || c == '\'') {
+                        //ignore these characters at the start of a word.
+                        depth++;
+                    } else {
+                        break;
+                    }
                 }
-                if (node.SortedKids[start].c == c) {
-                    node = node.SortedKids[start];
-                    depth++;
-                    if (node.ratio.isSet)
-                        result = node.ratio;
-                } else
-                    break;
-
-                if (depth > word.Length)
-                    break;
             }
-            return new AvsAn.Result(result.aCount, result.anCount, word, depth);
+            while (true) {
+                if (node.SortedKids == null) {
+                    break;
+                }
+                char c = depth == length ? ' ' : word[depth];
+                int candidateIdx = node.SortedKids.Length - 1;
+                int start = 0;
+                //invariant: only LT nodes before start
+                //invariant: only GTE nodes at or past candidateIdx *OR* needle doesn't exist.
+
+                while (candidateIdx != start) {
+                    int midpoint = candidateIdx + start >> 1;
+                    if (node.SortedKids[midpoint].c < c) {
+                        start = midpoint + 1;
+                    } else {
+                        candidateIdx = midpoint;
+                    }
+                }
+
+                if (node.SortedKids[candidateIdx].c == c) {
+                    node = node.SortedKids[candidateIdx];
+                    depth++;
+                    if (node.ratio.isSet) {
+                        result = node.ratio;
+                    }
+                } else {
+                    break;
+                }
+
+
+                if (depth > length) {
+                    break;
+                }
+            }
+            return new AvsAn.Result(result, word, depth);
         }
 
         public static AvsAn.PlainResult FastQuery(Node node, string word, int depth) {
             Ratio result = node.ratio;
-            while (true)
-                if (depth >= word.Length)
-                    return new AvsAn.PlainResult( result.AminAnDiff >= 0 ? AvsAn.IndefiniteArticle.A : AvsAn.IndefiniteArticle.An, word, depth);
-                else if (word[depth] == '(' || word[depth] == '\'' || word[depth] == '"') depth++;
-                else break;
             while (true) {
-                if (node.SortedKids == null) break;
+                if (depth >= word.Length) {
+                    return
+                        new AvsAn.PlainResult(
+                            result.AminAnDiff >= 0 ? AvsAn.IndefiniteArticle.A : AvsAn.IndefiniteArticle.An, word, depth);
+                } else if (word[depth] == '(' || word[depth] == '\'' || word[depth] == '"') {
+                    depth++;
+                } else {
+                    break;
+                }
+            }
+            while (true) {
+                if (node.SortedKids == null) {
+                    break;
+                }
                 char c = depth == word.Length ? ' ' : word[depth];
                 int start = 0, end = node.SortedKids.Length;
                 while (end - start > 1) {
                     int midpoint = end + start >> 1;
-                    if (node.SortedKids[midpoint].c <= c)
+                    if (node.SortedKids[midpoint].c <= c) {
                         start = midpoint;
-                    else
+                    } else {
                         end = midpoint;
+                    }
                 }
                 if (node.SortedKids[start].c == c) {
                     node = node.SortedKids[start];
                     depth++;
-                    if (node.ratio.isSet)
+                    if (node.ratio.isSet) {
                         result = node.ratio;
-                } else
+                    }
+                } else {
                     break;
+                }
 
-                if (depth > word.Length)
+                if (depth > word.Length) {
                     break;
+                }
             }
-            return new AvsAn.PlainResult(result.AminAnDiff >= 0 ? AvsAn.IndefiniteArticle.A : AvsAn.IndefiniteArticle.An, word, depth); 
+            return new AvsAn.PlainResult(
+                result.AminAnDiff >= 0 ? AvsAn.IndefiniteArticle.A : AvsAn.IndefiniteArticle.An, word, depth);
         }
     }
 }
