4cebe71360a67b702e363e34a7ea773ee2798c5a
Eamon Nerbonne <eamon@nerbonne.org>
2014-08-14 08:01:04 +0200

    Less functional, but shorter code for parsing.

--- A-vs-An/A-vs-An-DotNet/Internals/NodeDeserializer.cs
+++ A-vs-An/A-vs-An-DotNet/Internals/NodeDeserializer.cs
@@ -2,16 +2,7 @@ using System.Text;
 
 namespace AvsAnLib.Internals {
     public static class NodeDeserializer {
-        static ParseResult<T> Result<T>(int cursor, T value) {
-            return new ParseResult<T> { Cursor = cursor, Value = value };
-        }
-
-        struct ParseResult<T> {
-            public int Cursor;
-            public T Value;
-        }
-
-        static ParseResult<int> DenseIntParse(string str, int cursor) {
+        static int DenseIntParse(string str, ref int cursor) {
             int retval = 0;
             char c = str[cursor++];
             while (c != ';') {
@@ -19,36 +10,35 @@ namespace AvsAnLib.Internals {
                 retval += c - (c >= 'a' ? 'a' - 10 : '0');
                 c = str[cursor++];
             }
-            return Result(cursor, retval);
+            return retval;
         }
 
-        static ParseResult<Node> DeserializeDenseImpl(string rawDict, int cursor) {
-            var aCountResult = DenseIntParse(rawDict, cursor);
-            var anCountResult = DenseIntParse(rawDict, aCountResult.Cursor);
-            var kidCountResult = DenseIntParse(rawDict, anCountResult.Cursor);
-            int kidCount = kidCountResult.Value;
-            var ratio = new Ratio { aCount = aCountResult.Value, anCount = anCountResult.Value };
-            cursor = kidCountResult.Cursor;
+        static Node DeserializeDenseImpl(string rawDict, ref int cursor) {
+            var ratio = new Ratio { 
+                aCount = DenseIntParse(rawDict, ref cursor), 
+                anCount = DenseIntParse(rawDict, ref cursor) };
+           
+            int kidCount = DenseIntParse(rawDict, ref cursor);
             Node[] kids = null;
+            
             if (kidCount != 0) {
                 kids = new Node[kidCount];
                 for (int i = 0; i < kidCount; i++) {
-                    var c = rawDict[cursor];
-                    var nodeWithIdx = DeserializeDenseImpl(rawDict, cursor + 1);
-                    kids[i] = nodeWithIdx.Value;
+                    var c = rawDict[cursor++];
+                    var nodeWithIdx = DeserializeDenseImpl(rawDict, ref cursor);
+                    kids[i] = nodeWithIdx;
                     kids[i].c = c;
-                    cursor = nodeWithIdx.Cursor;
                 }
             }
-            return Result(cursor,
-                new Node {
-                    ratio = ratio,
-                    SortedKids = kids
-                });
+            return new Node {
+                ratio = ratio,
+                SortedKids = kids
+            };
         }
 
         public static Node DeserializeDense(string rawDict) {
-            return DeserializeDenseImpl(rawDict, 0).Value;
+            int cursor = 0;
+            return DeserializeDenseImpl(rawDict, ref cursor);
         }
     }
 }
