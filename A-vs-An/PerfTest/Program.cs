3e181f0aa804f9b8911513fb02e1740d503cfa95
Eamon Nerbonne <eamon@nerbonne.org>
2017-03-19 13:59:53 +0100

    Make perf-test more reasonable

--- A-vs-An/PerfTest/Program.cs
+++ A-vs-An/PerfTest/Program.cs
@@ -1,36 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Management;
 using AvsAnLib;
 using AvsAn_Test;
 
-namespace PerfTest {
-    static class Program {
-        static void Main(string[] args) {
+namespace PerfTest
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
             var words = Dictionaries.LoadEnglishDictionary();
-            
+            var borkedWords = words.Select(w => new string(w.Reverse().ToArray())).ToArray();
+            for (var i = 0; i < words.Length; i++) {
+                //deterministically shuffle to avoid over-friendliness to the branch-predictor
+                var otherI = (i * 13379L + 42) % words.Length;
+                var tmp = words[i];
+                words[i] = words[otherI];
+                words[otherI] = tmp;
+            }
+
             long sum = 0;
-            Stopwatch sw = Stopwatch.StartNew();
+            var sw = Stopwatch.StartNew();
             var _ = AvsAn.Query("example").Article;
             var init = sw.Elapsed;
             Console.WriteLine("initialization took " + init.TotalMilliseconds);
             sw.Restart();
-            const int iters = 400;
+            const int iters = 100;
             for (var k = 0; k < iters; k++) {
                 for (var i = 0; i < words.Length; i++)
                     sum += AvsAn.Query(words[i]).Article == "an" ? 1 : 0;
-                for (var i = words.Length - 1; i >= 0; i--)
-                    sum += AvsAn.Query(words[i]).Article == "an" ? 1 : 0;
+                for (var i = 0; i < borkedWords.Length; i++)
+                    sum += AvsAn.Query(borkedWords[i]).Article == "an" ? 1 : 0;
             }
             var duration = sw.Elapsed.TotalMilliseconds;
             var clockrateMHz =
                 new ManagementObjectSearcher(new ObjectQuery("SELECT CurrentClockSpeed FROM Win32_Processor")).Get()
-                .Cast<ManagementObject>().Select(mo => Convert.ToDouble(mo["CurrentClockSpeed"])).Max();
-            Console.WriteLine(sum + " / " + words.Length + " (" + ((double)sum / words.Length / iters / 2) + ") an rate.");
-            var microseconds = duration / words.Length / iters / 2 * 1000.0;
+                    .Cast<ManagementObject>()
+                    .Select(mo => Convert.ToDouble(mo["CurrentClockSpeed"]))
+                    .Max();
+            Console.WriteLine(sum + " / " + words.Length + " (" + (double)sum / words.Length / iters / 2 + ") an rate.");
+            var microseconds = duration / (words.Length + borkedWords.Length) / iters * 1000.0;
             Console.WriteLine(microseconds * 1000.0 + " nanoseconds per lookup");
 
             Console.WriteLine(clockrateMHz * microseconds + " cycles per lookup @ " + clockrateMHz + "MHz");
