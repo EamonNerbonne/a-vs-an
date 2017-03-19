a71c46c4194dd1e6f855c42a2723e791d38b1646
Eamon Nerbonne <eamon@nerbonne.org>
2017-03-19 14:51:12 +0100

    Remove unused usings

--- A-vs-An/WikipediaAvsAnTrieExtractor/RegexTextUtils.cs
+++ A-vs-An/WikipediaAvsAnTrieExtractor/RegexTextUtils.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using System.Linq;
 using System;
 using System.IO;
--- A-vs-An/WikipediaAvsAnTrieExtractorTest/MarkupStripperTests.cs
+++ A-vs-An/WikipediaAvsAnTrieExtractorTest/MarkupStripperTests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using ApprovalTests;
 using ApprovalTests.Approvers;
 using ApprovalTests.Core;
 using ApprovalTests.Reporters;
