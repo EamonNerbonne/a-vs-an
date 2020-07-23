c7d33003784feca78f40ebbe868466fb9fea0af6
Eamon Nerbonne <eamon@nerbonne.org>
2020-07-23 16:15:59 +0200

    Code cleanup: to expression bodied member; remove redundant "internal

--- A-vs-An/WikipediaAvsAnTrieExtractorTest/NodeEqualityComparer.cs
+++ A-vs-An/WikipediaAvsAnTrieExtractorTest/NodeEqualityComparer.cs
@@ -3,24 +3,20 @@ using System.Linq;
 using AvsAnLib.Internals;
 
 namespace WikipediaAvsAnTrieExtractorTest {
-    internal class NodeEqualityComparer : IEqualityComparer<Node> {
+    class NodeEqualityComparer : IEqualityComparer<Node> {
         public static readonly NodeEqualityComparer Instance = new NodeEqualityComparer();
 
-        public bool Equals(Node x, Node y) {
-            return
-                x.c == y.c
-                    && x.ratio.Occurrence == y.ratio.Occurrence
-                    && x.ratio.AminAnDiff == y.ratio.AminAnDiff
-                    && (x.SortedKids == null) == (y.SortedKids == null)
-                    && (x.SortedKids == null ||
-                        x.SortedKids.Length == y.SortedKids.Length
-                            && x.SortedKids.SequenceEqual(y.SortedKids, Instance)
-                        )
-                ;
-        }
+        public bool Equals(Node x, Node y)
+            => x.c == y.c
+                && x.ratio.Occurrence == y.ratio.Occurrence
+                && x.ratio.AminAnDiff == y.ratio.AminAnDiff
+                && (x.SortedKids == null) == (y.SortedKids == null)
+                && (x.SortedKids == null ||
+                    x.SortedKids.Length == y.SortedKids.Length
+                    && x.SortedKids.SequenceEqual(y.SortedKids, Instance)
+                );
 
-        public int GetHashCode(Node obj) {
-            return obj.c;
-        }
+        public int GetHashCode(Node obj)
+            => obj.c;
     }
 }
