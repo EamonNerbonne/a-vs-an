a2d1d52005624d1bfc14f4a1f6eeac809b9a3b69
Eamon Nerbonne <eamon@nerbonne.org>
2020-07-23 16:02:16 +0200

    Use correct order of equality assertion for better error message (xUnit analyzer)

--- A-vs-An/WikipediaAvsAnTrieExtractorTest/TriePrefixIncrementorTest.cs
+++ A-vs-An/WikipediaAvsAnTrieExtractorTest/TriePrefixIncrementorTest.cs
@@ -11,7 +11,7 @@ namespace WikipediaAvsAnTrieExtractorTest {
         public void BasicIncrementWorks() {
             var node = new Node();
             IncrementPrefixExtensions.IncrementPrefix(ref node, true, "test", 0);
-            Assert.Equal(NodeSerializer.Serialize(node), @"(0:1)t(0:1)te(0:1)tes(0:1)test(0:1)test (0:1)" );
+            Assert.Equal(@"(0:1)t(0:1)te(0:1)tes(0:1)test(0:1)test (0:1)" , NodeSerializer.Serialize(node));
         }
 
         [Fact]
@@ -19,7 +19,7 @@ namespace WikipediaAvsAnTrieExtractorTest {
             var node = new Node();
             IncrementPrefixExtensions.IncrementPrefix(ref node, true, "test", 0);
             IncrementPrefixExtensions.IncrementPrefix(ref node, true, "taste", 0);
-            Assert.Equal(NodeSerializer.Serialize(node), @"(0:2)t(0:2)ta(0:1)tas(0:1)tast(0:1)taste(0:1)taste (0:1)te(0:1)tes(0:1)test(0:1)test (0:1)");
+            Assert.Equal(@"(0:2)t(0:2)ta(0:1)tas(0:1)tast(0:1)taste(0:1)taste (0:1)te(0:1)tes(0:1)test(0:1)test (0:1)", NodeSerializer.Serialize(node));
         }
     }
 }
