2be594be3be7d2c9f358dd3cee7196c3e25fb815
Eamon Nerbonne <eamon@nerbonne.org>
2020-07-23 16:23:14 +0200

    Reformat

--- A-vs-An/A-vs-An-DotNet/Internals/Node.cs
+++ A-vs-An/A-vs-An-DotNet/Internals/Node.cs
@@ -8,7 +8,9 @@ namespace AvsAnLib.Internals {
         public char c;
         public Ratio ratio;
         public Node[] SortedKids;
-        public int CompareTo(Node other) { return c.CompareTo(other.c); }
+
+        public int CompareTo(Node other)
+            => c.CompareTo(other.c);
 
         public void LoadPrefixRatio(string prefix, int depth, Ratio prefixRatio) {
             if (prefix.Length == depth) {
@@ -24,13 +26,15 @@ namespace AvsAnLib.Internals {
             if (SortedKids == null) {
                 SortedKids = new[] { new Node { c = kidC, } }; //expensive, so many arrays.
                 return 0;
-            } else {
-                var idx = IdxAfterLastLtNode(kidC);
-
-                if (idx == SortedKids.Length || SortedKids[idx].c != kidC)
-                    InsertBefore(idx, kidC);
-                return idx;
             }
+
+            var idx = IdxAfterLastLtNode(kidC);
+
+            if (idx == SortedKids.Length || SortedKids[idx].c != kidC) {
+                InsertBefore(idx, kidC);
+            }
+
+            return idx;
         }
 
         int IdxAfterLastLtNode(char needle) {
@@ -42,11 +46,12 @@ namespace AvsAnLib.Internals {
                 var midpoint = end + start >> 1;
                 // start <= midpoint < end
                 if (SortedKids[midpoint].c < needle) {
-                    start = midpoint + 1;//i.e.  midpoint < start1 so start0 < start1
+                    start = midpoint + 1; //i.e.  midpoint < start1 so start0 < start1
                 } else {
-                    end = midpoint;//i.e end1 = midpoint so end1 < end0
+                    end = midpoint; //i.e end1 = midpoint so end1 < end0
                 }
             }
+
             return end;
         }
 
@@ -56,10 +61,12 @@ namespace AvsAnLib.Internals {
             for (; i < idx; i++) {
                 newArr[i] = SortedKids[i];
             }
+
             newArr[idx].c = newC;
             for (; i < SortedKids.Length; i++) {
                 newArr[i + 1] = SortedKids[i];
             }
+
             SortedKids = newArr;
         }
     }
--- A-vs-An/WikipediaAvsAnTrieExtractor/IncrementPrefixExtensions.cs
+++ A-vs-An/WikipediaAvsAnTrieExtractor/IncrementPrefixExtensions.cs
@@ -1,29 +1,37 @@
 ﻿using AvsAnLib.Internals;
 
-namespace WikipediaAvsAnTrieExtractor
-{
+namespace WikipediaAvsAnTrieExtractor {
     public static class IncrementPrefixExtensions {
         public static void IncrementPrefix(ref Node node, bool isAn, string word, int level) {
-            if (isAn) node.ratio.IncAn();
-            else node.ratio.IncA();
+            if (isAn) {
+                node.ratio.IncAn();
+            } else {
+                node.ratio.IncA();
+            }
 
-            if (level < 40)
+            if (level < 40) {
                 if (word.Length > level) {
                     var nextKidIdx = node.GetOrAddKidIdx(word[level]);
                     IncrementPrefix(
                         ref node.SortedKids[nextKidIdx],
-                        isAn, word, level + 1);
+                        isAn, word, level + 1
+                    );
                 } else if (word.Length == level) {
                     var nextKidIdx = node.GetOrAddKidIdx(' ');
                     IncrementTerminator(
                         ref node.SortedKids[nextKidIdx],
-                        isAn);
+                        isAn
+                    );
                 }
+            }
         }
 
         static void IncrementTerminator(ref Node node, bool isAn) {
-            if (isAn) node.ratio.IncAn();
-            else node.ratio.IncA();
+            if (isAn) {
+                node.ratio.IncAn();
+            } else {
+                node.ratio.IncA();
+            }
         }
     }
 }
