a17b3bda791ea5835d8554190b5b773289dd4bdf
Eamon Nerbonne <eamon@nerbonne.org>
2020-07-25 15:46:27 +0200

    Use same var-name as in  JS version

--- PerfTest/Program.cs
+++ PerfTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Linq;
 using System.Management;
+using System.Threading;
 using AvsAnLib;
 using AvsAnDemo;
 
@@ -9,17 +10,9 @@ namespace PerfTest
 {
     static class Program
     {
-        static void Main()
-        {
-            var words = Dictionaries.LoadEnglishDictionary();
-            var borkedWords = words.Select(w => new string(w.Reverse().ToArray())).ToArray();
-            for (var i = 0; i < words.Length; i++) {
-                //deterministically shuffle to avoid over-friendliness to the branch-predictor
-                var otherI = (i * 13379L + 42) % words.Length;
-                var tmp = words[i];
-                words[i] = words[otherI];
-                words[otherI] = tmp;
-            }
+        static void Main() {
+            var benchdict = Dictionaries.LoadEnglishDictionary();
+            var borkedWords = benchdict.Select(w => new string(w.Reverse().ToArray())).ToArray();
 
             long sum = 0;
             var sw = Stopwatch.StartNew();
@@ -29,7 +22,7 @@ namespace PerfTest
             sw.Restart();
             const int iters = 500;
             for (var k = 0; k < iters; k++) {
-                foreach (var word in words) {
+                foreach (var word in benchdict) {
                     sum += AvsAn.Query(word).Article == "an" ? 1 : 0;
                 }
 
@@ -43,8 +36,8 @@ namespace PerfTest
                     .Cast<ManagementObject>()
                     .Select(mo => Convert.ToDouble(mo["CurrentClockSpeed"]))
                     .Max();
-            Console.WriteLine(sum + " / " + words.Length + " (" + (double)sum / words.Length / iters / 2 + ") an rate.");
-            var microseconds = duration / (words.Length + borkedWords.Length) / iters * 1000.0;
+            Console.WriteLine(sum + " / " + benchdict.Length + " (" + (double)sum / benchdict.Length / iters / 2 + ") an rate.");
+            var microseconds = duration / (benchdict.Length + borkedWords.Length) / iters * 1000.0;
             Console.WriteLine(microseconds * 1000.0 + " nanoseconds per lookup");
 
             Console.WriteLine(clockrateMHz * microseconds + " cycles per lookup @ " + clockrateMHz + "MHz");
