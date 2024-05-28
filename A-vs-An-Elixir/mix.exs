20f11e6539c7421db0180778205fc7ec2a5bc2bc
Dave Howland <flux@Daves-MacBook-Pro.local>
2024-05-28 17:33:04 -0400

    update mix.exs for publishing

--- A-vs-An-Elixir/mix.exs
+++ A-vs-An-Elixir/mix.exs
@@ -2,6 +2,7 @@ defmodule AvsAn.MixProject do
   use Mix.Project
 
   @version "3.0.1"
+  @git_url "https://github.com/EamonNerbonne/a-vs-an"
 
   def project do
     [
@@ -9,7 +10,23 @@ defmodule AvsAn.MixProject do
       version: @version,
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: []
+      deps: [{:ex_doc, "~> 0.27", only: :dev, runtime: false}]
     ]
+  end
+
+  defp docs do
+    [
+      main: "AvsAn",
+      source_ref: "v#{@version}",
+      source_url: @git_url
+    ]
+  end
+
+  defp package do
+    %{
+      licenses: ["Apache-2.0"],
+      maintainers: ["David Howland"],
+      links: %{"GitHub" => @git_url}
+    }
   end
 end
